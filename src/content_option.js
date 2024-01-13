5a238305e97c455912232bc40c525824223e5196
mansardauna <olaomansur@gmail.com>
2024-01-13 09:32:42 -0500

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,170 @@
+const logotext = "MONSUR";
+const meta = {
+    title: "FrontEnd Developer",
+    description: "I’m Abdulrahman Monsur a frontend developer,with 3 years experince",
+};
+
+const introdata = {
+    title: "FrontEnd Developer",
+    animated: {
+        first: "I'm Abdulrahman Monsur",
+        second: "I develop responsive websites",
+        third: "I develop mobile apps",
+    },
+    description: "Resourceful Frontend software engineer with 3 years of experience designing,developing, and managing apps and internal frameworks",
+    your_img_url: "setup.jpeg",
+};
+
+const dataabout = {
+    title: "A bit about my self",
+    aboutme: "Resourceful Frontend software engineer with 3 years of experience designing, developing, and managing apps and internal frameworks. Specializes in ReactJS, Next Js, Typescript, and responsive design.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "ReactJS",
+        value: 90,
+    },
+    {
+        name: "Typescript",
+        value: 85,
+    },
+    {
+        name: "NodeJS",
+        value: 80,
+    },
+    {
+        name: "Tailwind CSS",
+        value: 60,
+    },
+    {
+        name: "Laravel",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "frontend delevopment",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Traning and Consaultancy",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "olaomansur@gmail.com",
+    YOUR_FONE: "(234)814-766-1430",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
