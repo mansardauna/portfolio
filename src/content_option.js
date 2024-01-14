dc98f7307d7c9fc5b246952ad2db930dbf1d94da
mansardauna <olaomansur@gmail.com>
2024-01-14 17:32:47 -0500

    done

--- src/content_option.js
+++ src/content_option.js
@@ -20,20 +20,16 @@ const dataabout = {
     aboutme: "Resourceful Frontend software engineer with 3 years of experience designing, developing, and managing apps and internal frameworks. Specializes in ReactJS, Next Js, Typescript, and responsive design.",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "Frontend Developer",
+        where: "CodeVillage LLC",
+        date: "2021",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
+        jobtitle: "Fukkstack Developer",
+        where: "Ankabit",
+        date: "2022",
     },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
-    },
+    
 ];
 
 const skills = [{
@@ -59,32 +55,32 @@ const skills = [{
 ];
 
 const services = [{
-        title: "frontend delevopment",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Frontend Delevopment",
+        description: "I major in the frontend aspect of software development, and with my experience and steadiness i can easily convert a design to Responsive and active app",
     },
     {
         title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        description: "Mobile application is fast growing in the software development, and it will be costless if same developer handles the web along side the mobile application, and as a client you won't need to be worried about the effectiveness",
     },
     {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Wordpress",
+        description: "Maintainance of a wordpress website or blog is the key to key it up and running, as a developer i can fix, build and maintain a wordpress or its plugins for you ",
     },
     {
         title: "Traning and Consaultancy",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        description: "to make work easier, it requires alot of helping hand and there was a statictics that developer are insufficient in the world. that is a lot of oppurtunity you can never miss if you start training today, its never to late. And if you have any I.T issues you can reach out to me. because I.T is the new reality ",
     },
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
+        img: "store.png",
         description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "https://monumental-shortbread-3dc8f7.netlify.app",
+        link: "https://online-store-chi-steel.vercel.app/",
     },
     {
-        img: "https://picsum.photos/400/800/?grayscale",
+        img: "usul.png",
         description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "https://harmonious-fairy-086a22.netlify.app",
+        link: "https://usuludeen.vercel.app"
     },
     {
         img: "https://picsum.photos/400/?grayscale",
@@ -94,7 +90,7 @@ const dataportfolio = [{
     {
         img: "https://picsum.photos/400/600/?grayscale",
         description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "https://symphonious-halva-004a77.netlify.app",
+        link: "https://monumental-shortbread-3dc8f7.netlify.app",
     }
 ];
 
@@ -102,18 +98,16 @@ const contactConfig = {
     YOUR_EMAIL: "olaomansur@gmail.com",
     YOUR_FONE: "(234)814-766-1430",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "service_id",
     YOUR_TEMPLATE_ID: "template_id",
     YOUR_USER_ID: "user_id",
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
+    github: "https://github.com/mansardauna",
+    facebook: "https://web.facebook.com/alao.mansur",
     linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+    twitter: "https://twitter.com/QuestyLLc",
 };
 export {
     meta,
