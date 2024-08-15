9ac6f052ba065e4980444250ef0dc8fa7fcec7ec
mansardauna <olaomansur@gmail.com>
2024-08-15 16:03:30 +0100

    modification

--- src/content_option.js
+++ src/content_option.js
@@ -1,123 +1,156 @@
 const logotext = "FRONTEND_M.A";
 const meta = {
-    title: "FrontEnd Developer",
-    description: "I’m Abdulrahman Monsur a frontend developer,with 3 years experince",
+  title: "FrontEnd Developer",
+  description:
+    "I’m Abdulrahman Monsur a frontend developer,with 3 years experince",
 };
 
 const introdata = {
-    title: "FrontEnd Developer",
-    animated: {
-        first: "I'm Abdulrahman Monsur",
-        second: "I develop responsive websites",
-        third: "I develop mobile apps",
-    },
-    description: "Resourceful Frontend software engineer with 3 years of experience designing,developing, and managing apps and internal frameworks",
-    your_img_url: "setup.jpeg",
+  title: "FrontEnd Developer",
+  animated: {
+    first: "I'm Abdulrahman Monsur",
+    second: "I develop responsive websites",
+    third: "I develop mobile apps",
+  },
+  description:
+    "Resourceful Frontend software engineer with 3 years of experience designing,developing, and managing apps and internal frameworks",
+  your_img_url: "setup.jpeg",
 };
 
 const dataabout = {
-    title: "A bit about my self",
-    aboutme: "Resourceful Frontend software engineer with 3 years of experience designing, developing, and managing apps and internal frameworks. Specializes in ReactJS, Next Js, Typescript, and responsive design.",
+  title: "A bit about my self",
+  aboutme: `      
+      Frontend Developer
+      Ankabit Technology — Oct 2022 - Nov 2023
+      
+      - Provided technical support during software installation and configuration.
+      - Played a key role in developing Einstein AI for Hubio Inc., Chicago, US.
+      - Led the development of a full-featured E-commerce web app.
+      - Built a functional Booking System for event centers and shortlets.
+      Frontend Developer
+      Code Village LLC — Sept 2021 - Aug 2022
+      
+      - Started as an intern and was later promoted to a full-time developer role.
+      - Developed and maintained front-end components in collaboration with the team.
+      - Implemented responsive designs and ensured cross-browser compatibility.
+      `,
 };
-const worktimeline = [{
-        jobtitle: "Frontend Developer",
-        where: "CodeVillage LLC",
-        date: "2021",
-    },
-    {
-        jobtitle: "Fukkstack Developer",
-        where: "Ankabit",
-        date: "2022",
-    },
-    
+const worktimeline = [
+  {
+    jobtitle: "Frontend Developer",
+    where: "CodeVillage LLC",
+    date: "2020-2022",
+  },
+  {
+    jobtitle: "Fullstack Developer",
+    where: "Ankabit",
+    date: "2022-2024",
+  },
 ];
 
-const skills = [{
-        name: "ReactJS",
-        value: 90,
-    },
-    {
-        name: "Typescript",
-        value: 85,
-    },
-    {
-        name: "NodeJS",
-        value: 80,
-    },
-    {
-        name: "Tailwind CSS",
-        value: 60,
-    },
-    {
-        name: "Laravel",
-        value: 85,
-    },
+const skills = [
+  {
+    name: "ReactJS",
+    value: 90,
+  },
+  {
+    name: "Typescript",
+    value: 85,
+  },
+  {
+    name: "Node JS",
+    value: 80,
+  },
+  {
+    name: "Alpine JS",
+    value: 80,
+  },
+  {
+    name: "Tailwind CSS",
+    value: 60,
+  },
+  {
+    name: "Laravel",
+    value: 85,
+  },
 ];
 
-const services = [{
-        title: "Frontend Delevopment",
-        description: "I major in the frontend aspect of software development, and with my experience and steadiness i can easily convert a design to Responsive and active app",
-    },
-    {
-        title: "Mobile Apps",
-        description: "Mobile application is fast growing in the software development, and it will be costless if same developer handles the web along side the mobile application, and as a client you won't need to be worried about the effectiveness",
-    },
-    {
-        title: "Wordpress",
-        description: "Maintainance of a wordpress website or blog is the key to key it up and running, as a developer i can fix, build and maintain a wordpress or its plugins for you ",
-    },
-    {
-        title: "Traning and Consaultancy",
-        description: "to make work easier, it requires alot of helping hand and there was a statictics that developer are insufficient in the world. that is a lot of oppurtunity you can never miss if you start training today, its never to late. And if you have any I.T issues you can reach out to me. because I.T is the new reality ",
-    },
+const services = [
+  {
+    title: "Frontend Delevopment",
+    description:
+      "I major in the frontend aspect of software development, and with my experience and steadiness i can easily convert a design to Responsive and active app",
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      "Mobile application is fast growing in the software development, and it will be costless if same developer handles the web along side the mobile application, and as a client you won't need to be worried about the effectiveness",
+  },
+  {
+    title: "Wordpress",
+    description:
+      "Maintainance of a wordpress website or blog is the key to key it up and running, as a developer i can fix, build and maintain a wordpress or its plugins for you ",
+  },
+  {
+    title: "Traning and Consaultancy",
+    description:
+      "to make work easier, it requires alot of helping hand and there was a statictics that developer are insufficient in the world. that is a lot of oppurtunity you can never miss if you start training today, its never to late. And if you have any I.T issues you can reach out to me. because I.T is the new reality ",
+  },
 ];
 
-const dataportfolio = [{
-        img: "store.png",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "https://online-store-chi-steel.vercel.app/",
-    },
-    {
-        img: "usul.png",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "https://usuludeen.vercel.app"
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "https://comfy-boba-22de80.netlify.app",
-    },
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "https://monumental-shortbread-3dc8f7.netlify.app",
-    }
+const dataportfolio = [
+  {
+    img: "store.png",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://online-store-chi-steel.vercel.app/",
+  },
+  {
+    img: "usul.png",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://usuludeen.vercel.app",
+  },
+  {
+    img: "potfolio.png",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://comfy-boba-22de80.netlify.app",
+  },
+  {
+    img: "wallpaper.png",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://monumental-shortbread-3dc8f7.netlify.app",
+  },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "olaomansur@gmail.com",
-    YOUR_FONE: "(234)814-766-1430",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+  YOUR_EMAIL: "olaomansur@gmail.com",
+  YOUR_FONE: "(234)814-766-1430",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
 };
 
 const socialprofils = {
-    github: "https://github.com/mansardauna",
-    facebook: "https://web.facebook.com/alao.mansur",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com/QuestyLLc",
+  github: "https://github.com/mansardauna",
+  facebook: "https://web.facebook.com/alao.mansur",
+  linkedin:
+    "https://www.linkedin.com/in/abdulrahman-mansur-284708213?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+  twitter: "https://twitter.com/QuestyLLc",
 };
 export {
-    meta,
-    dataabout,
-    dataportfolio,
-    worktimeline,
-    skills,
-    services,
-    introdata,
-    contactConfig,
-    socialprofils,
-    logotext,
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
 };
